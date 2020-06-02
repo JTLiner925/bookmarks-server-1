602628fed14d2ed2402d7073b2575a2acb82a356
JTLiner925 <[jtliner925@gmail.com]>
2020-06-02 12:33:02 -0500

    updating json file

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder-mifflin@localhost/bookmarks',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgres://cseyjqprfqoiax:bc9f678df932528ccf4fee7fc94db5fd563ec1f38e8283ee2f2b1b789a862f55@ec2-52-70-15-120.compute-1.amazonaws.com:5432/d52biacuneajk7',
 }
